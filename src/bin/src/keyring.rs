7c589dbb3f7374f8da64c098c2730b49e655450b
Asurar0 <Kayda0@proton.me>
2024-09-06 19:56:01 +0200

    Use `dirs` crate to fix default local folder for keyring

--- src/bin/src/keyring.rs
+++ src/bin/src/keyring.rs
@@ -4,7 +4,7 @@ use log::warn;
 use mikomikagi_keyring::backend::{LMDBKeyring, LMDBKeyringOptions};
 use mikomikagi_lib::Keyring;
 
-static DEFAULT_PATH: &str = "~/.mikomikagi.d/";
+static DEFAULT_PATH: &str = ".mikomikagi.d/";
 
 use crate::cli::Args;
 
@@ -13,7 +13,11 @@ pub fn init() -> Keyring<LMDBKeyring> {
     let args = Args::global();
     
     // Get path to keyring
-    let path = args.keyring_path.clone().unwrap_or(PathBuf::from(&DEFAULT_PATH));
+    let path = args.keyring_path.clone().unwrap_or({
+        let mut p = dirs::home_dir().unwrap();
+        p.push(PathBuf::from(&DEFAULT_PATH).as_path());
+        p
+    });
     
     // Check if the directory exist, otherwise log and initilialize
     if let Err(err) = fs::read_dir(&path) {
